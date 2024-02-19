22a3fe735f7810d82749fb60cfbe0629a85c6efd
Abyss <57658642+abyssxd@users.noreply.github.com>
2024-02-19 08:56:30 +0100

    Update status.js

--- public/js/status.js
+++ public/js/status.js
@@ -2,72 +2,76 @@
 const ws = new WebSocket('ws://localhost:3000');
 
 ws.onopen = function() {
-    console.log('WebSocket connection established');
+	console.log('WebSocket connection established');
 };
 
 ws.onerror = function(error) {
-    console.error('WebSocket Error:', error);
+	console.error('WebSocket Error:', error);
 };
 
 ws.onclose = function(event) {
-    console.log('WebSocket connection closed', event.code, event.reason);
+	console.log('WebSocket connection closed', event.code, event.reason);
 };
 
 
 /*TEST ---- START
 
-        system_status_data = 1;
-        bmp_status_data = 1;
-        gps_status_data = 0;
-        gyro_status_data = 0;
-        apc_status_data = 1;
-        
+bmp_status = 0;
+gps_status = 0;
+gyro_status = 0;
+apc_status = 0;
 
-        if(system_status_data != 1){
-            document.getElementById("sysBlock").style.backgroundColor = "#ff5c5c";
-        }else{
-            document.getElementById("sysBlock").style.backgroundColor = "#5cff5c";
-        }
-        
-        if (bmp_status_data != 1){
-            document.getElementById("bmpBlock").style.backgroundColor = "#ff5c5c";
-        }else{
-            document.getElementById("bmpBlock").style.backgroundColor = "#5cff5c";
-        }
-        
-        if (gps_status_data != 1){
-            document.getElementById("gpsBlock").style.backgroundColor = "#ff5c5c";
-        }else{
-            document.getElementById("gpsBlock").style.backgroundColor = "#5cff5c";
-        }
-        
-        if (gyro_status_data != 1){
-            document.getElementById("gyroBlock").style.backgroundColor = "#ff5c5c";
-        }else{
-            document.getElementById("gyroBlock").style.backgroundColor = "#5cff5c";
-        }
-        
-        if (apc_status_data != 1){
-            document.getElementById("apcBlock").style.backgroundColor = "#ff5c5c";
-        }else {
-            document.getElementById("apcBlock").style.backgroundColor = "#5cff5c";
-        }
+if (bmp_status != 1 && gps_status != 1 && gyro_status != 1 && apc_status != 1) {
+	system_status = 1;
+} else {
+	system_status = 0;
+}
 
 
-        if(system_status_data == 1 && bmp_status_data == 1 && gps_status_data == 1 && gyro_status_data == 1 && apc_status_data == 1){
-            document.getElementById("sysStatusText").innerHTML = "Online";
-            document.getElementById("sysBlock").style.backgroundColor = "#5cff5c";
-        } else if (system_status_data == 1 || bmp_status_data == 1 || gps_status_data == 1 || gyro_status_data == 1 || apc_status_data == 1){
-            document.getElementById("sysStatusText").innerHTML = "Partially Online";
-            document.getElementById("sysBlock").style.backgroundColor = "#ffd85c";
-        } else {
-            document.getElementById("sysStatusText").innerHTML = "Offline";
-            document.getElementById("sysBlock").style.backgroundColor = "#ff5c5c";
-        }
+if (system_status != 1) {
+	document.getElementById("sysBlock").style.backgroundColor = "#ff5c5c";
+} else {
+	document.getElementById("sysBlock").style.backgroundColor = "#5cff5c";
+}
+
+if (bmp_status != 1) {
+	document.getElementById("bmpBlock").style.backgroundColor = "#ff5c5c";
+} else {
+	document.getElementById("bmpBlock").style.backgroundColor = "#5cff5c";
+}
+
+if (gps_status != 1) {
+	document.getElementById("gpsBlock").style.backgroundColor = "#ff5c5c";
+} else {
+	document.getElementById("gpsBlock").style.backgroundColor = "#5cff5c";
+}
+
+if (gyro_status != 1) {
+	document.getElementById("gyroBlock").style.backgroundColor = "#ff5c5c";
+} else {
+	document.getElementById("gyroBlock").style.backgroundColor = "#5cff5c";
+}
+
+if (apc_status != 1) {
+	document.getElementById("apcBlock").style.backgroundColor = "#ff5c5c";
+} else {
+	document.getElementById("apcBlock").style.backgroundColor = "#5cff5c";
+}
 
 
-TEST ---- END
-*/
+if (bmp_status == 1 && gps_status == 1 && gyro_status == 1 && apc_status == 1) {
+	document.getElementById("sysStatusText").innerHTML = "Online";
+	document.getElementById("sysBlock").style.backgroundColor = "#5cff5c";
+} else if (bmp_status == 1 || gps_status == 1 || gyro_status == 1 || apc_status == 1) {
+	document.getElementById("sysStatusText").innerHTML = "Partially Online";
+	document.getElementById("sysBlock").style.backgroundColor = "#ffd85c";
+} else {
+	document.getElementById("sysStatusText").innerHTML = "Offline";
+	document.getElementById("sysBlock").style.backgroundColor = "#ff5c5c";
+}
+
+
+*/TEST ---- END
 
 
 
@@ -77,61 +81,65 @@ ws.onmessage = (event) => {
         const csvData = parseCSV(event.data);
         const latestData = csvData[csvData.length - 1];
     
-        system_status_data = latestData[9];
-        bmp_status_data = latestData[10];
-        gps_status_data = latestData[11];
-        gyro_status_data = latestData[12];
-        apc_status_data = latestData[13];
+        bmp_status = latestData[9];
+        gps_status = latestData[10];
+        gyro_status = latestData[11];
+        apc_status = latestData[12];
         
 
-        if(system_status_data != 1){
-            document.getElementById("sysBlock").style.display = "background-color: #ff5c5c";
-        }else{
-            document.getElementById("sysBlock").style.display = "background-color: #5cff5c";
+        if(bmp_status != 1 && gps_status != 1 && gyro_status != 1 && apc_status != 1){
+        system_status = 1;
+        } else {
+        system_status = 0;
         }
         
-        if (bmp_status_data != 1){
-            document.getElementById("bmpBlock").style.display = "background-color: #ff5c5c";
+
+        if(system_status != 1){
+            document.getElementById("sysBlock").style.backgroundColor = "#ff5c5c";
         }else{
-            document.getElementById("bmpBlock").style.display = "background-color: #5cff5c";
+            document.getElementById("sysBlock").style.backgroundColor = "#5cff5c";
         }
         
-        if (gps_status_data != 1){
-            document.getElementById("gpsBlock").style.display = "background-color: #ff5c5c";
+        if (bmp_status != 1){
+            document.getElementById("bmpBlock").style.backgroundColor = "#ff5c5c";
         }else{
-            document.getElementById("gpsBlock").style.display = "background-color: #5cff5c";
+            document.getElementById("bmpBlock").style.backgroundColor = "#5cff5c";
         }
         
-        if (gyro_status_data != 1){
-            document.getElementById("gyroBlock").style.display = "background-color: #ff5c5c";
+        if (gps_status != 1){
+            document.getElementById("gpsBlock").style.backgroundColor = "#ff5c5c";
         }else{
-            document.getElementById("gyroBlock").style.display = "background-color: #5cff5c";
+            document.getElementById("gpsBlock").style.backgroundColor = "#5cff5c";
         }
         
-        if (apc_status_data != 1){
-            document.getElementById("apcBlock").style.display = "background-color: #ff5c5c";
+        if (gyro_status != 1){
+            document.getElementById("gyroBlock").style.backgroundColor = "#ff5c5c";
+        }else{
+            document.getElementById("gyroBlock").style.backgroundColor = "#5cff5c";
+        }
+        
+        if (apc_status != 1){
+            document.getElementById("apcBlock").style.backgroundColor = "#ff5c5c";
         }else {
-            document.getElementById("apcBlock").style.display = "background-color: #5cff5c";
+            document.getElementById("apcBlock").style.backgroundColor = "#5cff5c";
         }
 
 
-        if(system_status_data == 1 && bmp_status_data == 1 && gps_status_data == 1 && gyro_status_data == 1 && apc_status_data == 1){
-            document.getElementById("sysBlock").innerHTML = "Online";
-            document.getElementById("sysBlock").style.display = "background-color: #5cff5c";
-        } else if (system_status_data != 1 || bmp_status_data != 1 || gps_status_data != 1 || gyro_status_data != 1 || apc_status_data != 1){
-            document.getElementById("sysBlock").style.display = "Partially Online";
-            document.getElementById("sysBlock").style.display = "background-color: #ffd85c";
-        } else{
-            document.getElementById("sysBlock").style.display = "Offline";
-            document.getElementById("sysBlock").style.display = "background-color: #ff5c5c";
+        if(bmp_status == 1 && gps_status == 1 && gyro_status == 1 && apc_status == 1){
+            document.getElementById("sysStatusText").innerHTML = "Online";
+            document.getElementById("sysBlock").style.backgroundColor = "#5cff5c";
+        } else if (bmp_status == 1 || gps_status == 1 || gyro_status == 1 || apc_status == 1){
+            document.getElementById("sysStatusText").innerHTML = "Partially Online";
+            document.getElementById("sysBlock").style.backgroundColor = "#ffd85c";
+        } else {
+            document.getElementById("sysStatusText").innerHTML = "Offline";
+            document.getElementById("sysBlock").style.backgroundColor = "#ff5c5c";
         }
-        
 
 };
 
-
 //Function to parse the CSV & tirm the values. This should work both on windows and linux.
 function parseCSV(csvString) {
-    const rows = csvString.trim().split(/\r?\n/);
-    return rows.map(row => row.split(',').map(cell => cell.trim()));
+	const rows = csvString.trim().split(/\r?\n/);
+	return rows.map(row => row.split(',').map(cell => cell.trim()));
 }
